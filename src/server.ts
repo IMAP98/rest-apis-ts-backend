a4550d88d1e439213eb97221c727dfccaefc50c4
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-10-17 10:55:35 -0600

    test

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ const server = express();
 // NOTE: Allow connections
 const corsOptions: CorsOptions = {
     origin: (origin, callback) => {
-        if (origin === process.env.FRONTEND_URL || !origin) {
+        if (origin === process.env.FRONTEND_URL) {
             callback(null, true);
         } else {
             callback(new Error('CORS error'));
